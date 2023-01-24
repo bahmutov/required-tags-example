219560938fb7ddb5bcc5dc712afe85e3e11c51b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-23 21:38:57 -0500

    add specs

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  env: { grepFilterSpecs: true, grepOmitFiltered: true },
   e2e: {
     // baseUrl, etc
-    supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // and load any plugins that require the Node environment
+      require('@bahmutov/cy-grep/src/plugin')(config)
+      // IMPORTANT: return the config object
+      return config
     },
-  }
+  },
 })
