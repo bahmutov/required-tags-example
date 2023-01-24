adf4a3aaf66efbf5fdc7bdf9c994e23f39f5d46b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-24 13:46:55 -0500

    try running specs in order

--- cypress.config.js
+++ cypress.config.js
@@ -5,12 +5,17 @@ module.exports = defineConfig({
   e2e: {
     // baseUrl, etc
     fixturesFolder: false,
+    specPattern: [
+      'cypress/e2e/spec-b.cy.js',
+      'cypress/e2e/scrape.cy.js',
+      'cypress/e2e/clean.cy.js',
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // and load any plugins that require the Node environment
-      require('@bahmutov/cy-grep/src/plugin')(config)
+      // require('@bahmutov/cy-grep/src/plugin')(config)
       // IMPORTANT: return the config object
-      return config
+      // return config
     },
   },
 })
